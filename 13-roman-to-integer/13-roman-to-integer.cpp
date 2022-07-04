8fbae3e292ea61ba52d3a3841823151d0c5c83e4
Ankit Kumar <72691866+ayeankit@users.noreply.github.com>
2022-07-04 23:32:47 +0530

    Time: 33 ms (16.72%), Space: 8.3 MB (14.31%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,103 +1,30 @@
 class Solution {
 public:
+    
+        void map(unordered_map<char, int> &mp)
+        {
+        mp.insert({'I',1});
+        mp.insert({'V',5});
+        mp.insert({'X',10});                                      
+        mp.insert({'L',50});
+        mp.insert({'C',100});
+        mp.insert({'D',500});
+        mp.insert({'M',1000});
+    }
+
     int romanToInt(string s) {
-        
-        //first taking IV,IX,XL,XD, XL-40, XC-90, CD-400, CM-900
-        
-        
-  int last_read = 0;
-        int ans = 0;
-        for(int i = 0;i < s.length()-1;i++){
-            if(s[i] == 'I' && s[i+1] == 'V'){
-                if(i == s.length()-2){
-                    last_read = 1;
-                }
-                ans += 4;
-                i++;
-            }
-            else if(s[i] == 'I' && s[i+1] == 'X'){
-                if(i == s.length()-2){
-                    last_read = 1;
-                }
-                ans += 9;
-                i++;
-            }
-            else if(s[i] == 'X' && s[i+1] == 'L'){
-                if(i == s.length()-2){
-                    last_read = 1;
-                }
-                ans += 40;
-                i++;
-            }
-            else if(s[i] == 'X' && s[i+1] == 'C'){
-                if(i == s.length()-2){
-                    last_read = 1;
-                }
-                ans += 90;
-                i++;
-            }
-            else if(s[i] == 'C' && s[i+1] == 'D'){
-                if(i == s.length()-2){
-                    last_read = 1;
-                }
-                ans += 400;
-                i++;
-            }
-            else if(s[i] == 'C' && s[i+1] == 'M'){
-                if(i == s.length()-2){
-                    last_read = 1;
-                }
-                ans += 900;
-                i++;
-            }
-            else{
-                if(s[i] == 'I'){
-                    ans += 1;
-                }
-                else if(s[i] == 'V'){
-                    ans += 5;
-                }
-                else if(s[i] == 'X'){
-                    ans += 10;
-                }
-                else if(s[i] == 'L'){
-                    ans += 50;
-                }
-                else if(s[i] == 'C'){
-                    ans += 100;
-                }
-                else if(s[i] == 'D'){
-                    ans += 500;
-                }
-                else if(s[i] == 'M'){
-                    ans += 1000;
-                }
+        unordered_map<char, int> mp;
+        map(mp);
+        int result = 0;
+        for (int i = 0; i <= s.length()-1; i++) {
+            if (mp[s[i]] < mp[s[i+1]]) {
+                result -= mp[s[i]];
+            } else {
+                result += mp[s[i]];
             }
         }
-        if(last_read == 0){
-            int i = s.length()-1;
-            if(s[i] == 'I'){
-                    ans += 1;
-                }
-                else if(s[i] == 'V'){
-                    ans += 5;
-                }
-                else if(s[i] == 'X'){
-                    ans += 10;
-                }
-                else if(s[i] == 'L'){
-                    ans += 50;
-                }
-                else if(s[i] == 'C'){
-                    ans += 100;
-                }
-                else if(s[i] == 'D'){
-                    ans += 500;
-                }
-                else if(s[i] == 'M'){
-                    ans += 1000;
-                }
-        }
-        return ans;
+        return result;
     }
 };
+        
+   
