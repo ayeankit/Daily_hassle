b737bafc5074b9cfaaceef7d5d4250c6d21f83ac
Ankit Kumar <72691866+ayeankit@users.noreply.github.com>
2022-07-05 21:46:08 +0530

    Time: 3 ms (92.46%), Space: 6.6 MB (55.98%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-   
+    string countAndSay(int n) {
         
-  string countAndSay(int n) {
-    if(n==1) return "1";
-    if(n==2) return "11";
-    string s = countAndSay(n-1);   
-    string res = "";
-    int count = 0;
-      for(int i=0;i<s.length();i++){
-        count++;
-          if(i==s.length()-1 || s[i]!=s[i+1] ){
-              res =res+ to_string(count) +s[i];
-              count = 0;
-          }
-        }   
-        return res;
+   if(n == 1)
+    {
+       return "1"; 
     }
+    
+	//By recursion we got the ans of (n-1)th string
+    string s = countAndSay(n-1);
+    
+	//So now, we will find the answer for the given n by using "s" which stores the (n-1)th countAndSay.
+	
+    string ans ;
+	
+	//we will check for the times a digit appeared in s and put it in count and a prev to count number of times that digit apeared in a row.
+    int count = 1;
+    char prev = s[0];
+    
+	
+    for(int i=1; i<s.size(); i++)
+    {
+	    //if we found the digit repeating we will just increase its count.
+        if(prev == s[i])
+        {
+            count++;
+            continue;
+        }
+        
+		//else we will update our answer.
+        ans += count + '0' ;
+        ans += prev;
+        
+        prev = s[i];
+        count = 1;
+    } 
+    
+    ans += count + '0' ;
+    ans += prev;
+    
+    
+    return ans;
+    
+}
 };
