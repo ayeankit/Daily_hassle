00b08a53becc2ec550b7afd45c34e2e545d3a6e7
Ankit Kumar <72691866+ayeankit@users.noreply.github.com>
2022-07-12 02:05:39 +0530

    Added solution - LeetHub

---
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// Function to return minimum number of jumps to end of array
+
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+      
+        if(n<=1)
+       {
+           return 0;
+       }
+       
+       if(arr[0]==0)   return -1;
+       
+       int jump=0;
+       int position=0;
+       int maxReach=0;
+       
+       for(int i=0;i<n-1;i++)
+       {
+           maxReach=max(maxReach,i+arr[i]);
+           
+           if(i==position)
+           {
+               jump++;
+               
+               if(i>=maxReach) return -1;
+               
+               position=maxReach;
+           }
+       }
+       
+       return jump;
+   }
+       
+    
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,j;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution obj;
+        cout<<obj.minJumps(arr, n)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
