32864ca5a0519c851eb0d414fde69442b203c8cb
Ankit Kumar <72691866+ayeankit@users.noreply.github.com>
2022-07-21 14:49:48 +0530

    Time: 425 ms (29.29%), Space: 114 MB (85.56%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,27 +11,31 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-          
-        vector<int> vec;
-        
-        ListNode *curr = head;
-        
-        while(curr!=NULL){
-            vec.push_back(curr->val);
-            curr = curr->next;
-        }
-        
-        int low = 0,high = vec.size()-1;
-        
-        while(low<=high){
-            
-            if(vec[low] != vec[high]){
-                return false;
-            }else{
-                low++;
-                high--;
-            }
-        }
-        return true;
+        if(!head || !head->next) return true;
+    ListNode *slow = head, *fast = head->next;
+    while(fast && fast->next) {//split into two halves while the first half can be one-node longer;
+        slow = slow->next;
+        fast = fast->next->next;
     }
+    fast = slow->next;
+    slow->next = NULL;
+    ListNode newHead(0); //reverse the second half;
+    ListNode *next = NULL, *p = fast;
+    while(p) {
+        next = p->next;
+        p->next = newHead.next;
+        newHead.next = p;
+        p = next;
+    }
+    fast = newHead.next; //compare the two lists;
+    while(fast) {
+        if(fast->val != head->val) return false;
+        fast = fast->next;
+        head = head->next;
+    }
+    return fast == NULL;
+}
+
+       
+
 };
